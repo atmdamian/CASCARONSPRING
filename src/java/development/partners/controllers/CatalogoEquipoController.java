dc57be421379761bd00df066a0e3bf0556a4694e
damian.atanacio <damian.atanacio@RNS-AMD.dominionmx.com.mx>
2019-02-08 16:12:40 -0600

    Merge origin/master
    Conflicts:
    	web/WEB-INF/jsp/catalogos/detallesCatalogo.jsp

---
+++ src/java/development/partners/controllers/CatalogoEquipoController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package development.partners.controllers;
+
+import development.partners.models.Equipo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author damian.atanacio
+ */
+@Controller
+public class CatalogoEquipoController {
+    @RequestMapping(value="catalogoEquipos", method=RequestMethod.GET)
+    public ModelAndView verCatalogoEquipos(){
+        
+        return new ModelAndView("catalogos/catalogoEquipos","command",new Equipo());
+    }
+    
+    @ModelAttribute("listaMarcas")
+    public Map<String, String> getListaMarcas() {
+        Map<String, String> listaMarcas = new HashMap<>();
+        listaMarcas.put("lv", "Lenovo");
+        listaMarcas.put("hp", "HP");
+        listaMarcas.put("ac", "Acer");
+        listaMarcas.put("app", "Apple");
+        listaMarcas.put("th", "Toshiba");
+        listaMarcas.put("sm", "Samsung");
+        listaMarcas.put("gt", "Gateway");
+        listaMarcas.put("cq", "Compaq");
+        listaMarcas.put("sy", "Sony");
+        listaMarcas.put("lg", "LG");
+        listaMarcas.put("lx", "Lanix");
+        listaMarcas.put("ibm", "IBM");
+        listaMarcas.put("pn", "panasonic");
+        listaMarcas.put("aw", "Alienware");
+        return listaMarcas;
+    }
+    @ModelAttribute("listaSO")
+    public Map<String, String> getSistemaOperativo(){
+    Map<String, String> listaSO = new HashMap<>();
+    listaSO.put("xp", "Windows XP" );
+    listaSO.put("wv", "Windows Vista");
+    listaSO.put("w7", "Windows 7");
+    listaSO.put("w8", "Windows 8");
+    listaSO.put("w10", "Windows 10");
+            
+    return listaSO;
+    }
+    
+    
+    
+    @ModelAttribute("capacidadHD")
+    public List<String> getCapacidadHD(){
+    List<String> capacidadHD = new ArrayList<>();
+    capacidadHD.add("320 GB");
+    capacidadHD.add("500 GB");
+    capacidadHD.add("1 TB");
+    
+    return capacidadHD;
+    }
+    @ModelAttribute("velocidadRam")
+    public List<String> getVelocidaRam(){
+        List<String> velocidadRam = new ArrayList<>();        
+        velocidadRam.add("4 ");
+        velocidadRam.add("8 ");
+        velocidadRam.add("16 Gb");
+    return velocidadRam;
+    }
+}
+
---
+++ web/WEB-INF/jsp/catalogos/catalogoEquipos.jsp
@@ -0,0 +1,63 @@
+<%-- 
+    Document   : catalogoEquipos
+    Created on : 8/02/2019, 01:18:43 PM
+    Author     : damian.atanacio
+--%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+         pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="sf"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <sf:form>
+            <div>
+                <sf:label path="no_serie" >NO. Serie:</sf:label>
+                <sf:input path="no_serie"/>
+            </div>
+            <div>
+
+                <sf:label path="marca">Marca:</sf:label>
+                <sf:select path="marca">
+                    <sf:option value="NADA" label="Selecciona Marca" />
+                    <sf:options items="${listaMarcas}" />
+                </sf:select>                   
+            </div>
+            <div>
+                <sf:label path="modelo">Modelo:</sf:label>
+                <sf:input path="modelo" />
+            </div>
+            <div>
+                <sf:label path="so">Sistema Operativo:</sf:label>
+                <sf:select path="so" >
+                    <sf:option value="NADA" label="Selecciona S.O"/>
+                    <sf:options items="${listaSO}" />
+                </sf:select>    
+            </div>
+                <div>
+                <sf:label path="hd">Capacidad HD:</sf:label>
+                <sf:radiobuttons path="hd" items="${capacidadHD}"/>
+                </div>
+                <div>
+                    <sf:label path="ram">Velocidad Ram</sf:label>
+                    <sf:radiobuttons path="ram" items="${velocidadRam}" />
+                </div>
+                <div>
+                    <sf:label path="ip">Direccion IP:</sf:label>
+                    <sf:input path="ip" />
+                </div>
+                <div>
+                    <div>
+                    <sf:label path="estado">Estado Equipo:</sf:label>
+                    </div>
+                    <sf:textarea path="estado" rows="5" cols="30" />                                 
+                </div>
+        </sf:form>
+    </body>
+</html>
